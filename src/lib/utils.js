d5ab8cb5d71fc1c3184aa9e0bfcfee7bc83b5ca8
HimanshuxAI <himanshusharma70066@gmail.com>
2026-06-14 16:14:26 +0530

    docs: document tailwind merge utility function in utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,10 @@
 import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * Utility function to merge Tailwind CSS classes with clsx.
+ * Resolves style conflicts.
+ */
 export function cn(...inputs) {
     return twMerge(clsx(inputs));
 }
