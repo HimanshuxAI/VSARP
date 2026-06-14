677f9b988c0f7b8b4ea6d67492799eaafa55421e
HimanshuxAI <himanshusharma70066@gmail.com>
2026-06-14 16:27:54 +0530

    Add lint:fix script to enforce code style

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    // Add any custom rules here
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
